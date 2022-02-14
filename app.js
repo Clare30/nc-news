5fd21b03fa18cd9c2e26fb954c419c4ab45577d8
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-14 14:16:21 +0000

    add app.all to handle universal 404 error

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const app = express();
 module.exports = app;
 
 app.get("/api/topics", controllers.topics.getTopics);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "path does not exist" });
+});
