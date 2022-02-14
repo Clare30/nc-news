c1cfc2dadf978f12ecbd630436b546f3a30549ac
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-14 13:29:14 +0000

    create app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const controllers = require("./controllers");
+const controllers = require("./controllers/");
 
 const app = express();
 
 module.exports = app;
+
+app.get("/api/topics", controllers.topics.getTopics);
