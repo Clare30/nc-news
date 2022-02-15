56f3b04ecc2382eac7936ac78a69eff3b9ec9f1f
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-15 14:34:12 +0000

    update controller with catch

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,10 @@
 const models = require("../models/index");
 
 exports.getUsers = (req, res, next) => {
-  return models.users.selectUsers().then((users) => {
-    res.status(200).send({ users });
-  });
+  return models.users
+    .selectUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch((err) => next(err));
 };
