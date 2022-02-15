4e144f85c3076ea73009cd39b01c853e2ddacc74
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-15 12:05:41 +0000

    update app.js for update article

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const controllers = require("./controllers/");
 const errors = require("./controllers/errors.controller");
 
 const app = express();
+app.use(express.json());
 
 module.exports = app;
 
 app.get("/api/topics", controllers.topics.getTopics);
 app.get("/api/articles/:article_id", controllers.articles.getArticleById);
+app.patch("/api/articles/:article_id", controllers.articles.updateArticle);
 
 app.use(errors.handleCustoms);
 app.use(errors.handlePsqlErrors);
