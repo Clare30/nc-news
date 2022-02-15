5b71c8e425a98be209750804b9b1af7baaea5b7c
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-15 12:05:20 +0000

    add model for update article

--- models/articles.model.js
+++ models/articles.model.js
@@ -2,10 +2,19 @@ const db = require("../db/connection.js");
 
 exports.selectArticleById = async (id) => {
   const { rows } = await db.query(
-    "SELECT * FROM articles WHERE article_id = $1",
+    "SELECT * FROM articles WHERE article_id = $1;",
     [id]
   );
   if (rows.length === 0)
     return Promise.reject({ status: 404, msg: "article does not exist" });
   else return rows[0];
+};
+
+exports.amendArticle = async (id, votes) => {
+  const { rows } = await db.query(
+    "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;",
+    [votes, id]
+  );
+
+  return rows[0];
 };
