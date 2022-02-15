60824721948dc785baa2fa88d3c5ec3443405d88
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-15 10:43:33 +0000

    amend controller to include catch

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,9 +1,14 @@
-const { selectArticleById } = require("../models/articles.model");
 const models = require("../models/index");
 
-exports.getArticleById = (req, res) => {
+exports.getArticleById = (req, res, next) => {
   const id = req.params.article_id;
-  return models.articles.selectArticleById(id).then((article) => {
-    res.status(200).send({ article });
-  });
+
+  return models.articles
+    .selectArticleById(id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
