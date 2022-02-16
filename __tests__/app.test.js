dcc69ad359967482c3d629aa99be4c90c72d7e06
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-16 12:14:59 +0000

    test for get comments by article ID

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -183,29 +183,29 @@ describe("/api/articles/:article_id", () => {
   });
 });
 
-// describe("/api/articles/:article_id/comments", () => {
-//   describe("GET", () => {
-//     test("status: 200 - responds with an array of comments for the given article ID", () => {
-//       return request(app)
-//         .get("/api/articles/1/comments")
-//         .expect(200)
-//         .then(({ body: { comments } }) => {
-//           expect(comments).toHaveLength(11);
-//           comments.forEach((comment) => {
-//             expect(comment).toEqual(
-//               expect.objectContaining({
-//                 comment_id: expect.any(Number),
-//                 votes: expect.any(Number),
-//                 created_at: expect.any(String),
-//                 author: expect.any(String),
-//                 body: expect.any(String),
-//               })
-//             );
-//           });
-//         });
-//     });
-//   });
-// });
+describe("/api/articles/:article_id/comments", () => {
+  describe("GET", () => {
+    test("status: 200 - responds with an array of comments for the given article ID", () => {
+      return request(app)
+        .get("/api/articles/1/comments")
+        .expect(200)
+        .then(({ body: { comments } }) => {
+          expect(comments).toHaveLength(11);
+          comments.forEach((comment) => {
+            expect(comment).toEqual(
+              expect.objectContaining({
+                comment_id: expect.any(Number),
+                votes: expect.any(Number),
+                created_at: expect.any(String),
+                author: expect.any(String),
+                body: expect.any(String),
+              })
+            );
+          });
+        });
+    });
+  });
+});
 
 describe("/api/users", () => {
   describe("GET", () => {
