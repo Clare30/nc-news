2b91a825d9ee6ebf6dd8d79abf6d888e6fe7788d
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-16 11:13:21 +0000

    updated model to add comment_count column

--- models/articles.model.js
+++ models/articles.model.js
@@ -9,13 +9,16 @@ exports.selectArticles = async () => {
 
 exports.selectArticleById = async (id) => {
   const { rows } = await db.query(
-    "SELECT * FROM articles WHERE article_id = $1;",
+    "SELECT articles.*, COUNT(comments.article_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;",
     [id]
   );
+
   if (rows.length === 0)
     return Promise.reject({ status: 404, msg: "article does not exist" });
   return rows[0];
 };
+
+
 
 exports.amendArticle = async (id, votes) => {
   const { rows } = await db.query(
