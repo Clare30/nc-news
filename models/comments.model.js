ac22c11201ea1e237000b10ebffb7644f0e40b21
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-16 12:16:20 +0000

    model for comments

---
+++ models/comments.model.js
@@ -0,0 +1,9 @@
+const db = require("../db/connection.js");
+
+exports.selectCommentsByArticleId = async (id) => {
+  const { rows } = await db.query(
+    "SELECT * FROM comments WHERE article_id = $1",
+    [id]
+  );
+  return rows;
+};
