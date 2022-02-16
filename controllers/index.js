c11612808c9dee8547f8bcdf42ed7af7567ffd67
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-16 12:15:51 +0000

    index updated for new comment controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 exports.topics = require("./topics.controller");
 exports.articles = require("./articles.controller");
 exports.users = require("./users.controller");
+exports.comments = require("./comments.controller");
 exports.errors = require("./errors.controller");
