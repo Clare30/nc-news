dbe5a87fae1d0c7dbec03b2d0a1a795a07197eaa
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-02-17 11:12:48 +0000

    seperate out comment_count test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -69,7 +69,6 @@ describe("/api/articles", () => {
                 topic: expect.any(String),
                 created_at: expect.any(String),
                 votes: expect.any(Number),
-                comment_count: expect.any(Number),
               })
             );
           });
@@ -81,6 +80,26 @@ describe("/api/articles", () => {
         .expect(200)
         .then(({ body: { articles } }) => {
           expect(articles).toBeSortedBy("created_at", { descending: true });
+        });
+    });
+    test("returns array of article objects with comment_count property", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body: { articles } }) => {
+          articles.forEach((article) => {
+            expect(article).toEqual(
+              expect.objectContaining({
+                author: expect.any(String),
+                title: expect.any(String),
+                article_id: expect.any(Number),
+                topic: expect.any(String),
+                created_at: expect.any(String),
+                votes: expect.any(Number),
+                comment_count: expect.any(Number),
+              })
+            );
+          });
         });
     });
   });
